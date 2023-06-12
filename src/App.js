bbcde9f9f00c33edbd01f9a588edb49dc32a664e
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-13 00:01:37 +0800

    login and register pages added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Post from "./Post";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import Loginpage from "./pages/LoginPage";
+import Registerpage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +11,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<Loginpage />} />
+        <Route path='/register' element={<Registerpage />} />
+
       </Route>
     </Routes>
   );
