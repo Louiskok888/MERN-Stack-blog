6a69017264fc320ef3324372096e50968b8abc10
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-12 16:18:22 +0800

    homepage created with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   <div>test</div>
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Blogs for all day</h2>
+          <p className="info">
+          <a className="author">Louis Kok</a>
+          <time>2023-06-12 16.08</time>
+          </p>
+          <p className="summary">
+            A blog, short for weblog, is a frequently updated web page used for
+            personal commentary or business content. Blogs are often interactive
+            and include sections at the bottom of individual blog posts where
+            readers can leave comments.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Blogs for all day</h2>
+          <p className="info">
+          <a className="author">Louis Kok</a>
+          <time>2023-06-12 16.08</time>
+          </p>
+          <p className="summary">
+            A blog, short for weblog, is a frequently updated web page used for
+            personal commentary or business content. Blogs are often interactive
+            and include sections at the bottom of individual blog posts where
+            readers can leave comments.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://img.freepik.com/free-photo/online-message-blog-chat-communication-envelop-graphic-icon-concept_53876-139717.jpg"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>Blogs for all day</h2>
+          <p className="info">
+          <a className="author">Louis Kok</a>
+          <time>2023-06-12 16.08</time>
+          </p>
+          <p className="summary">
+            A blog, short for weblog, is a frequently updated web page used for
+            personal commentary or business content. Blogs are often interactive
+            and include sections at the bottom of individual blog posts where
+            readers can leave comments.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
-
 export default App;
