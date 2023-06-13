a1edcb4d4e9dbc039506cb981412e61b2dc397b2
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-13 23:37:04 +0800

    jwt,cookie set up

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,35 +1,45 @@
-import { useState } from "react"
-
+import { useState } from "react";
+import { Navigate} from "react-router-dom"
 
 export default function Loginpage() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false)
 
-    async function login(ev) {
-        ev.preventDefault()
-        await fetch('http://localhost:4000/login', {
-            nethod: 'POST',
-            body: JSON.stringify({username, password}),
-            headers: {'content-type':'application/json'},
-        })
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "content-type": "application/json" },
+      credentials: 'include',
+    });
+    if (response.ok) {
+        setRedirect(true)
+    } else {
+        alert('wrong credential')
     }
-    return(
-        <form className="login" onSubmit={login}>
-        <h1>Login</h1>
-        <input 
-        type="text" 
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input
+        type="text"
         placeholder="username"
         value={username}
-        onChange={ev => setUsername(ev.target.value)}
-        />
-        <input 
-        type="password" 
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
         placeholder="password"
         value={password}
-        onChange={ev => setPassword(ev.target.value)}
-        />
-        <button>Login</button>
-        </form>
-
-    )
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>Login</button>
+    </form>
+  );
 }
