d7e73d35424ea67f1718c01b794a164cfd6f991b
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-13 17:22:42 +0800

    login request created

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,33 @@
+import { useState } from "react"
+
+
 export default function Loginpage() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function login(ev) {
+        ev.preventDefault()
+        await fetch('http://localhost:4000/login', {
+            nethod: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'content-type':'application/json'},
+        })
+    }
     return(
-        <form className="login">
+        <form className="login" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input 
+        type="text" 
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}
+        />
+        <input 
+        type="password" 
+        placeholder="password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}
+        />
         <button>Login</button>
         </form>
 
