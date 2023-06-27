550c27bc0608022b625a00bde7dc7c521bcfeb07
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-27 09:10:35 +0800

    create post page modified

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,5 +1,48 @@
-export default function CreatePost(){
-    return(
-        <div>here you create new post</div>
-    )
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  async function createNewPost(ev) {
+    const data = new FormData()
+    data.set('title', title)
+    data.set('summary', summary)
+    data.set('content', content)
+    data.set('file', files[0])
+    ev.preventDefault()
+    const response = await fetch('http://localhost:4000/post', {
+        method: 'POST',
+        body: data,
+    })
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={ev => setSummary(ev.target.value)}
+      />
+      <input 
+        type="file" 
+        onChange={ev => setFiles(ev.target.value)}
+      />
+      <ReactQuill
+        value={content}
+        onChange={newValue => setContent(newValue)}
+      />
+      <button style={{ marginTop: "5px" }}>Create Post</button>
+    </form>
+  );
 }
