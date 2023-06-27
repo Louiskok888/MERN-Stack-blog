a8df1a855235db628003577d257fe67a8aef9c7c
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-27 11:16:31 +0800

    model for post created

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,26 +1,34 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 
 export default function CreatePost() {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
   async function createNewPost(ev) {
-    const data = new FormData()
-    data.set('title', title)
-    data.set('summary', summary)
-    data.set('content', content)
-    data.set('file', files[0])
-    ev.preventDefault()
-    console.log(files)
-    const response = await fetch('http://localhost:4000/post', {
-        method: 'POST',
-        body: data,
-    })
-  }
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+    ev.preventDefault();
 
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+ 
   return (
     <form onSubmit={createNewPost}>
       <input
@@ -33,15 +41,12 @@ export default function CreatePost() {
         type="summary"
         placeholder={"Summary"}
         value={summary}
-        onChange={ev => setSummary(ev.target.value)}
+        onChange={(ev) => setSummary(ev.target.value)}
       />
-      <input 
-        type="file" 
-        onChange={ev => setFiles(ev.target.files)}
-      />
+      <input type="file" onChange={(ev) => setFiles(ev.target.files)} />
       <ReactQuill
         value={content}
-        onChange={newValue => setContent(newValue)}
+        onChange={(newValue) => setContent(newValue)}
       />
       <button style={{ marginTop: "5px" }}>Create Post</button>
     </form>
