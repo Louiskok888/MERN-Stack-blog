48026c5542cab09d24f6eafd6546d63cba5bec27
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-06-28 14:52:58 +0800

    posts added at main page

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,4 @@
-export default function Post() {
+export default function Post({title,summary,cover,content,createdAt}) {
   return (
     <div className="post">
       <div className="image">
@@ -8,16 +8,13 @@ export default function Post() {
         />
       </div>
       <div className="texts">
-        <h2>Blogs for all day</h2>
+        <h2>{title}</h2>
         <p className="info">
           <a className="author">Louis Kok</a>
-          <time>2023-06-12 16.08</time>
+          <time>{createdAt}</time>
         </p>
         <p className="summary">
-          A blog, short for weblog, is a frequently updated web page used for
-          personal commentary or business content. Blogs are often interactive
-          and include sections at the bottom of individual blog posts where
-          readers can leave comments.
+          {summary}
         </p>
       </div>
     </div>
